28033d1a6f31314a84aae6af6cd2d7309df38cf2
Samuel <samueldivine2000@gmail.com>
2023-07-06 07:24:04 -0700

    Rx

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,5 @@
 #!/usr/bin/env ruby
 
-# Get the argument passed to the script
-input = ARGV[0]
-
-# Define the regular expression pattern to match "School"
-pattern = /School/
-
-# Match the pattern against the input string
-match = input.match(pattern)
-
-# Output the matched string or an empty string if no match is found
-puts match ? match[0] : ""
-
 require 're'
 
 def match_school(string)
